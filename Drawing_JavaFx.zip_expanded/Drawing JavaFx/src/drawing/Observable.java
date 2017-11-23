ceda63c83f0edb8430221f1360b906f5aac3b502
lducoroy <l.ducoroy@gmail.com>
2017-11-23 08:50:26 +0100

    Ajout de la deuxième évolution terminée. Suppression de la première version

--- Drawing_JavaFx.zip_expanded/Drawing JavaFx/src/drawing/Observable.java
+++
@@ -1,7 +0,0 @@
-package drawing;
-
-public interface Observable {
-	public void ajouterObservateur(Observateur o);
-	public void supprimerObservateur(Observateur o);
-	public void notifierObservateurs();
-}
--- Drawing_JavaFx.zip_expanded/Drawing JavaFx/src/drawing/Observateur.java
+++
@@ -1,5 +0,0 @@
-package drawing;
-
-public interface Observateur {
-	public void actualiser(Observable o);
-}
